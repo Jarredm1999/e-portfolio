69a665b4fa101ea951e678c506c79d45147e4ca3
Jarred Myers <jarredm1999@gmail.com>
2025-09-11 21:29:45 -0700

    Added portfolio information

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,12 +4,21 @@ function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
       <h1 className="text-4xl md:text-7xl text-[#78A083] mb-1 md:mb-3 font-bold">
-        Jarred
+        Jarred Myers
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">
         Software Engineer & Aspiring Security Professional
       </p>
-      <p className="text-sm max-w-xl mb-6 font-bold">This is my bio...</p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        I am currently a Software Engineer trying to transistion into
+        Cybersecurity. With a strong foundation in programming and system
+        design, I'm building on my techinical background to focus on cloud and
+        security. I hold a CompTIA Security+ certification and am currently
+        pursuing Microsoft Azure Fundamentals(AZ-900) to deepen my cloud
+        security expertise. My goal is to apply both my engineering skills and
+        security knowledge to help organizations build and protect modern
+        systems.
+      </p>
     </div>
   );
 }
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,32 +1,37 @@
 export default [
   {
-    year: "Today",
-    title: "Software Engineer",
+    year: "Aug 2022 - Today",
+    title: "Google - Software Engineer",
     duration: "3 years",
-    details: "I worked hehe",
+    details:
+      "At Google, I build and optimize Android components for Google Maps, improving stability and reliability for millions of users. I also helped develop a headless navigation API, streamlined debugging workflows to cut resolution time by 50%, and led end to end projects.",
   },
   {
     year: "Jan 2022 - May 2022",
-    title: "Software Engineer",
+    title: "Anglepoint - Software Dev",
     duration: "5 Months",
-    details: "I worked hehe",
+    details:
+      "At Anglepoint, I enhanced the Elevate web platform by building features that improved stability, performance, and user experience. I collaborated in agile sprints and code reviews to deliver scalable, business aligned solutions.",
   },
   {
     year: "May 2020 - Aug 2020",
-    title: "Software Engineer Intern",
+    title: "Google - Software Engineer Intern",
     duration: "4 months",
-    details: "I worked hehe",
+    details:
+      "Refactored outdated Google Shopping front end codebase into updated Google owned fornt end frameworks, improving maintainability.",
   },
   {
     year: "May 2019 - Aug 2019",
-    title: "Engineering Practicum Intern",
+    title: "Google - Engineering Practicum Intern",
     duration: "4 months",
-    details: "I worked hehe",
+    details:
+      "Migrated Android Mobile Maps navigation components to align with Google's new material design standards, enhancing UI consistency and usability.",
   },
   {
     year: "May 2018 - Aug 2018",
-    title: "Engineering Practicum Intern",
+    title: "Google - Engineering Practicum Intern",
     duration: "4 months",
-    details: "I worked hehe",
+    details:
+      "Developed Google My Business API website code samples to enhance developer onboarding integration time.",
   },
 ];
