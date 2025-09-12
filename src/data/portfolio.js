f6b97b041b0ba300058032480b1afb964bfceab9
Jarred Myers <jarredm1999@gmail.com>
2025-09-12 10:59:36 -0700

    test

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,23 +1,23 @@
 export default [
-  // {
-  //   title: "Terminal Website",
-  //   imgUrl: "/assets/IMG_0659FB.jpeg",
-  //   desc: "This is a small desc...",
-  //   stack: ["HTML", "CSS", "JavaScript"],
-  //   link: "https://fkcodes.com",
-  // },
-  // {
-  //   title: "Cloud Project",
-  //   imgUrl: "/assets/IMG_0659FB.jpeg",
-  //   desc: "This is a small desc...",
-  //   stack: ["HTML", "CSS", "JavaScript"],
-  //   link: "https://fkcodes.com",
-  // },
-  // {
-  //   title: "Cloud Security Project",
-  //   imgUrl: "/assets/IMG_0659FB.jpeg",
-  //   desc: "This is a small desc...",
-  //   stack: ["HTML", "CSS", "JavaScript"],
-  //   link: "https://fkcodes.com",
-  // },
+  {
+    title: "Terminal Website",
+    imgUrl: "/assets/IMG_0659FB.jpeg",
+    desc: "This is a small desc...",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://fkcodes.com",
+  },
+  {
+    title: "Cloud Project",
+    imgUrl: "/assets/IMG_0659FB.jpeg",
+    desc: "This is a small desc...",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://fkcodes.com",
+  },
+  {
+    title: "Cloud Security Project",
+    imgUrl: "/assets/IMG_0659FB.jpeg",
+    desc: "This is a small desc...",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://fkcodes.com",
+  },
 ];
