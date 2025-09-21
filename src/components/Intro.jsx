82fd9e7cfe603bff9cd301f6bc87f73f4e4edefd
Jarred Myers <jarredm1999@gmail.com>
2025-09-21 15:38:50 -0700

    add az-900 update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -16,8 +16,8 @@ function Intro() {
         I am currently a Software Engineer trying to transistion into
         Cybersecurity. With a strong foundation in programming and system
         design, I'm building on my techinical background to focus on cloud and
-        security. I hold a CompTIA Security+ certification and am currently
-        pursuing Microsoft Azure Fundamentals(AZ-900) to deepen my cloud
+        security. I hold a Google Cybersecurity certification, CompTIA Security+ 
+        certification and Microsoft Azure Fundamentals(AZ-900) to deepen my cloud
         security expertise. My goal is to apply both my engineering skills and
         security knowledge to help organizations build and protect modern
         systems.
--- src/data/certifications.js
+++ src/data/certifications.js
@@ -2,13 +2,18 @@
 
 export default [
   {
+    logo: "/e-portfolio/assets/google_cert_logo.png",
+    name: "Google Cybersecurity",
+    link: "https://drive.google.com/file/d/1M_i0sNmPlVRGLV4HQacfX4KidICQXUSE/view?usp=sharing",
+  },
+  {
     logo: "/e-portfolio/assets/securityplus.png",
     name: "CompTIA Security+",
     link: "https://drive.google.com/file/d/1oKLMCl8XL1J0XarCFYUN2JLSVdXjKSJp/view?usp=sharing",
   },
-  // {
-  //   logo: "/e-portfolio/assets/az900.png",
-  //   name: "Azure Fundamentals (AZ-900)",
-  //   link: "https://drive.google.com/file/d/1oKLMCl8XL1J0XarCFYUN2JLSVdXjKSJp/view?usp=sharing",
-  // },
+  {
+    logo: "/e-portfolio/assets/az900.png",
+    name: "Azure Fundamentals (AZ-900)",
+    link: "https://drive.google.com/file/d/10p6yuIsezovO8lQlLXY7PrP1RwRADY2Z/view?usp=sharing",
+  },
 ];
