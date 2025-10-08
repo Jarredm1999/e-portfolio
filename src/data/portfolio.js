c92a1bb88f46c73fcc8d85cbd7d3585e43327c81
Jarred Myers <jarredm1999@gmail.com>
2025-10-07 22:36:53 -0700

    added aws vpc project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,6 +2,13 @@
 
 export default [
   {
+    title: "AWS Secure VPC + IDS",
+    imgUrl: "/e-portfolio/assets/ld-final-diagram.png",
+    desc: "Created a secure VPC architecture, created IDS with metrics and alerts.",
+    stack: ["AWS", "VPC", "EC2", "GuardDuty", "FlowLogs", "CloudWatch"],
+    link: "https://github.com/Jarredm1999/Secure-VPC-with-IDS",
+  },
+  {
     title: "Azure Honeypot + Attack Map",
     imgUrl: "/e-portfolio/assets/ld-final-diagram.png",
     desc: "Used Azure to plumb Microsoft Security Events to a Map.",
